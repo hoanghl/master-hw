fec203c5698a6996ed940738ba51d2366cc92346
Hoang Le <tommyle133@gmail.com>
2024-04-07 17:16:00 +0300

    Finish all TMC assignment in ex2

--- hy-advanced-ml-2024/part2-01.nb/src/nb.py
+++ hy-advanced-ml-2024/part2-01.nb/src/nb.py
@@ -1,45 +1,77 @@
 import sys
+
 import numpy as np
 
 
 def nb(X, labels):
-	"""
-	Computes the weight vector w and and bias b corresponding
-	to the Naive Bayes classifier with Bernoulli components.
+    """
+    Computes the weight vector w and and bias b corresponding
+    to the Naive Bayes classifier with Bernoulli components.
 
-	Parameters
-	----------
-	X : an array of size (n, k)
-		training input data for the classifier, elements must be 0/1
-	labels : an array of size n
-		training labels for the classifier, elements must be 0/1
+    Parameters
+    ----------
+    X : an array of size (n, k)
+            training input data for the classifier, elements must be 0/1
+    labels : an array of size n
+            training labels for the classifier, elements must be 0/1
 
-	Returns
-	-------
-	w : an array of size k
-		weights corresponding to the classifier
-	bias: real number
-		bias term corresponding to the classifier
-	"""
+    Returns
+    -------
+    w : an array of size k
+            weights corresponding to the classifier
+    bias: real number
+            bias term corresponding to the classifier
+    """
 
-	cnt, k = X.shape
-	w = np.zeros(k)
-	b = 0
-	# place your code here
-	return w, b
+    cnt, k = X.shape
+    w = np.zeros(k)
+    b = 0
+
+    # Estimate the parameters in NB
+    theta1 = {j: 0 for j in range(k)}
+    theta2 = {j: 0 for j in range(k)}
+    pi = 0
+
+    for i in range(cnt):
+        # Count pi
+        if labels[i] == 1:
+            pi += 1
+
+        # Count theta
+        theta = theta1 if labels[i] == 1 else theta2
+        for j in range(k):
+            if X[i, j] == 1:
+                theta[j] += 1
+    for j in range(k):
+        theta1[j], theta2[j] = theta1[j] / pi, theta2[j] / (cnt - pi)
+    pi = pi / cnt
+
+    # Estimate w and b
+    s = 0
+    for j in range(k):
+        w[j] = (
+            np.log(theta1[j])
+            - np.log(1 - theta1[j])
+            + np.log(1 - theta2[j])
+            - np.log(theta2[j])
+        )
+
+        s += np.log(1 - theta1[j]) - np.log(1 - theta2[j])
+    b = np.log(pi) - np.log(1 - pi) + s
+
+    return w, b
 
 
 def main(argv):
-	D = np.loadtxt(argv[1])
-	X = D[:, 1:]
-	labels = D[:,0]
-	print(nb(X, labels))
-
+    D = np.loadtxt(argv[1])
+    X = D[:, 1:]
+    labels = D[:, 0]
+    print(nb(X, labels))
 
 
 # This allows the script to be used as a module and as a standalone program
-if __name__ == "__main__": 
-	if len(sys.argv) != 2:
-		print('usage: python %s filename' % sys.argv[0])
-	else:
-		main(sys.argv)
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("usage: python %s filename" % sys.argv[0])
+    else:
+        main(sys.argv)
--- hy-advanced-ml-2024/part2-03.lasso/src/lasso.py
+++ hy-advanced-ml-2024/part2-03.lasso/src/lasso.py
@@ -1,58 +1,88 @@
 import sys
 import numpy as np
 
+def get_cd(X, y, w, d):
+    N = X.shape[0]
+    
+    result = 0
+    for n in range(N):
+        X_notd = np.concatenate([X[n, :d], X[n, d+1:]], axis=-1)
+        w_notd = np.concatenate([w[:d], w[d+1:]], axis=-1)
+
+        result += X[n, d] * (y[n] - (X_notd[None, :] @ w_notd[:, None]).squeeze().item())
+    result = result * 2
+    return result
 
 def lasso(X, y, s, reg, itercnt):
-	"""
-	Subgradient optimization for Lasso 
+    """
+    Subgradient optimization for Lasso 
 
-	Parameters
-	----------
-	X : an array of size (n, k)
-		training input data for the regressor
-	y : an array of size n
-		training output for the regressor
-	s : an array of size k
-		initial weights
-	reg : real
-		weight for the regularization term
-	itercnt : int
-		number of iterations
+    Parameters
+    ----------
+    X : an array of size (n, k)
+        training input data for the regressor
+    y : an array of size n
+        training output for the regressor
+    s : an array of size k
+        initial weights
+    reg : real
+        weight for the regularization term
+    itercnt : int
+        number of iterations
 
-	Returns
-	-------
-	w : an array of size k
-		weights after itercnt iterations
-	"""
+    Returns
+    -------
+    w : an array of size k
+        weights after itercnt iterations
+    """
 
-	cnt, k = X.shape
+    cnt, k = X.shape
 
-	# make a copy of the initial vector
-	# we can now change single elements of w without changing s
-	w = s.copy()
+    # make a copy of the initial vector
+    # we can now change single elements of w without changing s
+    w = s.copy()
 
-	# place your code here
+    # place your code here
+    D = w.shape[0]
 
-	return w
+    # Calculate a
+    a = np.zeros((D,))
+    for d in range(D):
+        a[d] = 2 * np.sum(np.square(X[:, d]))
+
+    for _ in range(itercnt):
+        for d in range(D):
+            cd = get_cd(X, y, w, d)
+            print("cd: ", cd)
+            if d == 0:
+                w[d] = cd / a[d]
+            else:
+                if cd <= -reg:
+                    w[d] = (cd + reg) / a[d]
+                elif -reg <= cd <= reg:
+                    w[d] = 0
+                else:
+                    w[d] = (cd - reg) / a[d]
+    return w
 
 
 def main(argv):
-	D = np.loadtxt(argv[1])
-	y = D[:,0].copy() # copy is needed, otherwise next line will mess up the splice
-	D[:,0] = 1 # replace the output column of D with constant, now the first feature gives us the bias term
+    D = np.loadtxt(argv[1])
+    y = D[:,0].copy() # copy is needed, otherwise next line will mess up the splice
+    D[:,0] = 1 # replace the output column of D with constant, now the first feature gives us the bias term
 
-	reg = float(argv[2])
-	itercnt = int(argv[3])
+    reg = float(argv[2])
+    itercnt = int(argv[3])
 
-	w = np.zeros(D.shape[1]) # starting point for weights
+    w = np.zeros(D.shape[1]) # starting point for weights
 
-	print(lasso(D, y, w, reg, itercnt))
+    print(lasso(D, y, w, reg, itercnt))
 
 
 
 # This allows the script to be used as a module and as a standalone program
 if __name__ == "__main__": 
-	if len(sys.argv) != 4:
-		print('usage: python %s filename' % sys.argv[0])
-	else:
-		main(sys.argv)
+    if len(sys.argv) != 4:
+        print('usage: python %s filename' % sys.argv[0])
+    else:
+        main(sys.argv)
